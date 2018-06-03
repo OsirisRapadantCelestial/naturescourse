911e3a4791d972216c819c4c48729ca9c130c23e
Anthony Fuller <anthony.fuller.gmod@gmail.com>
2018-06-03 20:18:29 +0100

    Looks nicer

--- gamemode/shared/sh_caneat.lua
+++ gamemode/shared/sh_caneat.lua
@@ -12,15 +12,19 @@ local foodtype = {
 }
 
 function plymeta:CanEat( foodtype )
-    if foodtype == "meat" && self.stats["eat"] == "Carnivore" || self.stats["eat"] == "Omnivore" then
+    if foodtype == "meat" && self.stats["eat"] == "Carnivore" then
         return true
     else
         return false
     end
 
-    if foodtype == "plant" && self.stats["eat"] == "Herbivore" || self.stats["eat"] == "Omnivore" then
+    if foodtype == "plant" && self.stats["eat"] == "Herbivore" then
         return true
     else
         return false
+    end
+
+    if self.stats["eat"] == "Omnivore" then
+        return true
     end
 end
