d771d191ff1eb5b449d36e2882733125eca934ef
Anthony Fuller <anthony.fuller.gmod@gmail.com>
2018-06-03 20:05:15 +0100

    Actually added CanEat

--- gamemode/shared/sh_caneat.lua
+++ gamemode/shared/sh_caneat.lua
@@ -1,11 +1,26 @@
 local plymeta = FindMetaTable( "Player" )
 
 local foodtype = {
-    ["Carnivore"] = {
-        ["Meat"] = true,
+    ["meat"] = {
+        ["Carnivore"] = true,
+        ["Omnivore"] = true,
+    },
+    ["plant"] = {
+        ["Herbivore"] = true,
+        ["Omnivore"] = true,
     },
 }
 
 function plymeta:CanEat( foodtype )
+    if foodtype == "meat" && self.stats["eat"] == "Carnivore" || self.stats["eat"] == "Omnivore" then
+        return true
+    else
+        return false
+    end
 
+    if foodtype == "plant" && self.stats["eat"] == "Herbivore" || self.stats["eat"] == "Omnivore" then
+        return true
+    else
+        return false
+    end
 end
