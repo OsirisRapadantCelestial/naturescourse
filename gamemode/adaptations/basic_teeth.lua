c783849758a5f38a3fbf869b6b274ce07fda2131
AnthonyFuller <anthony.fuller.gmod@gmail.com>
2018-07-30 17:29:05 +0100

    Added basic teeth template for adaptations

---
+++ gamemode/adaptations/basic_teeth.lua
@@ -0,0 +1,33 @@
+local tbl = {}
+tbl.Name = "Basic Teeth"
+tbl.Model = "models/hunter/blocks/cube025x05x025.mdl"
+tbl.type = "Teeth"
+tbl.desc = "Just a bit gummy."
+tbl.default = true
+tbl.slots = {
+	[1] = {
+		pos = Vector(0,0, 10),
+		ang = Angle(0,0,0),
+	}
+}
+tbl.effects = {
+	["HUD"] = function(self)
+
+	end,
+	["Skill"] = function(self)
+
+	end,
+	["Damage"] = function(self)
+
+	end,
+	["Death"] = function(self)
+
+	end,
+	["Evolve"] = function(self)
+
+	end,
+	["Teeth"] = function(self)
+
+	end,
+}
+RegisterAdaptation(tbl)
